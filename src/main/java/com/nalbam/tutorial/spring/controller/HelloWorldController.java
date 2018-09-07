af8ee22175c800cfcdf3c1196c41fe4cc6d54b7a
nalbam <me@nalbam.com>
2018-09-07 18:15:00 +0900

    profile

--- src/main/java/com/nalbam/tutorial/spring/controller/HelloWorldController.java
+++ src/main/java/com/nalbam/tutorial/spring/controller/HelloWorldController.java
@@ -1,10 +1,10 @@
 package com.nalbam.tutorial.spring.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
@@ -14,10 +14,18 @@ import java.util.TimeZone;
 @Controller
 public class HelloWorldController {
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @RequestMapping(path = {"/"}, method = RequestMethod.GET)
     public String index(Model model) {
+        // profile
+        model.addAttribute("profile", profile);
+
+        // message
         model.addAttribute("message", "Hello Spring MVC!");
 
+        // host
         String host;
         try {
             host = InetAddress.getLocalHost().getHostName();
@@ -27,6 +35,7 @@ public class HelloWorldController {
         }
         model.addAttribute("host", host);
 
+        // date
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
         model.addAttribute("date", sdf.format(new Date()));
